a841bf6997a6d6e02415a38e0ba060e1e6ce090c
Tony Mogoa <mogoa.tonny@gmail.com>
2021-11-02 20:38:48 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 #include <GL/glut.h>
-void displayMe(void)
+
+void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
     glBegin(GL_POLYGON);
+
+    glColor3f(1.0f, 0.0f, 0.0f);
     glVertex3f(0.0, 0.0, 0.0);
-    glVertex3f(0.5, 0.0, 0.0);
+
+    glColor3f(0.0f, 1.0f, 0.0f);
     glVertex3f(0.5, 0.5, 0.0);
+
+    glColor3f(1.0f, 1.0f, 0.0f);
     glVertex3f(0.0, 0.5, 0.0);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.5, 0.0, 0.0);
+
     glEnd();
     glFlush();
 }
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(300, 300);
+    glutInitWindowSize(800, 800);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("SAMPLE TEST");
-    glutDisplayFunc(displayMe);
+    glutCreateWindow("Coloured Polygon");
+    glutDisplayFunc(display);
     glutMainLoop();
     return 0;
 }
